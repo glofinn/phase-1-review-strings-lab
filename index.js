427b37b0d09d024e193ee299fde7a949e148ac2b
Finn Chen <finn.chen.maenhaut@gmail.com>
2023-01-30 15:40:09 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = "Finn";
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
